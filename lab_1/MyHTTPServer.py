cd14e7407f6c890f6c555b8f0d200d1560ed32cc
Roman Triputko <romantriputko@gmail.com>
2021-02-02 22:26:34 +0300

    fix response with error

--- lab_1/MyHTTPServer.py
+++ lab_1/MyHTTPServer.py
@@ -3,9 +3,11 @@ from http.request import Request
 from http.response import Response
 from http.httpError import HTTPError
 from email.parser import Parser
+import os
 
 MAX_LINE = 64 * 1024
 MAX_HEADERS = 100
+
 
 class MyHTTPServer:
     def __init__(self, host, port, server_name):
@@ -57,7 +59,7 @@ class MyHTTPServer:
             raise HTTPError(400, 'Bad request', 'Host header is missing')
         if host not in (self._server_name, f'{self._host}:{self._port}'):
             raise HTTPError(404, 'Not found')
-        
+
         return Request(method, target, ver, headers, rfile)
 
     def parse_headers(self, rfile):
@@ -92,7 +94,6 @@ class MyHTTPServer:
             raise HTTPError(505, 'HTTP Version Not Supported')
         return method, target, ver
 
-    
     def handle_request(self, req):
         if req.method == 'GET':
             return self.handle_get(req)
@@ -105,16 +106,20 @@ class MyHTTPServer:
 
     def handle_get(self, req):
         filename = req.target
+        file_path = 'files/' + filename
+
+        if not os.path.isfile(file_path):
+            raise HTTPError(404, 'Not found')
 
         try:
-            fin = open('files/' + filename)
+            fin = open(file_path)
             body = fin.read()
             fin.close()
 
             body = body.encode('utf-8')
-            headers = [('Content-Length', len(body))]
+            headers = {'Content-Length': len(body)}
             return Response(200, 'OK', headers, body)
-        
+
         except FileNotFoundError:
             raise HTTPError(404, 'Not found')
 
@@ -123,14 +128,14 @@ class MyHTTPServer:
 
     def handle_options(self, req):
         pass
-    
+
     def send_response(self, conn, resp):
         wfile = conn.makefile('wb')
         status_line = f'HTTP/1.1 {resp.status} {resp.reason}\r\n'
         wfile.write(status_line.encode('iso-8859-1'))
 
         if resp.headers:
-            for (key, value) in resp.headers:
+            for key, value in resp.headers.items():
                 header_line = f'{key}: {value}\r\n'
                 wfile.write(header_line.encode('iso-8859-1'))
 
@@ -151,5 +156,5 @@ class MyHTTPServer:
             status = 500
             reason = b'Internal Server Error'
             body = b'Internal Server Error'
-        resp = Response(status, reason, ['Content-Length', len(body)], body)
+        resp = Response(status, reason, {'Content-Length': len(body)}, body)
         self.send_response(conn, resp)
