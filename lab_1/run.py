3b616f17e14260a9d709a795c4c4813e3c836796
Roman Triputko <romantriputko@gmail.com>
2021-02-21 16:12:58 +0300

    refactor default host

--- lab_1/run.py
+++ lab_1/run.py
@@ -7,7 +7,7 @@ def get_args():
     argument_parser = argparse.ArgumentParser(description=f'Server settings')
 
     argument_parser.add_argument('-p', '--port', help='Server port', type=int, default=8000)
-    argument_parser.add_argument('-ht', '--host', help='Server host', type=str, default='localhost')
+    argument_parser.add_argument('-ht', '--host', help='Server host', type=str, default='127.0.0.1')
     argument_parser.add_argument('-n', '--name', help='Server name', type=str, default='example.local')
 
     args = argument_parser.parse_args()
